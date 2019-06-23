2bad87b7de7074b799ad48910c8c1a3b8594a9b4
inosoma <inosoma@github.com>
2019-06-23 23:47:53 +0300

    Barcode v1.0

---
+++ Barcode/Barcode/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zen.Barcode;
+
+namespace Barcode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public BarcodeDraw bdraw = BarcodeDrawFactory.GetSymbology(BarcodeSymbology.Code128);
+        public Image barcodeImage;
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public PartialViewResult Barcode(string barcodeno = "")
+        {
+            if (barcodeno == "")
+            {
+                 barcodeno = "100";
+            }
+            Session["BarcodeNo"] = barcodeno;
+            barcodeImage = bdraw.Draw(barcodeno, 40, 2);
+            MemoryStream ms = new MemoryStream();
+            barcodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            byte[] byteData = ms.ToArray();
+            Session["barcodeimagebyte"] = byteData;
+            return PartialView();
+        }
+
+
+    }
+}
